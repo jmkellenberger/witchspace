afdfa6faef6dcb764add0862594be5ae53ddca68
Jack Kellenberger <jackson@kellenberger.io>
2023-02-07 14:17:50 -0500

    Implement basic CLI with clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
-mod rng;
-use rng::{rng_from_seed, Pcg64, Rollable};
+use clap::Parser;
+use rand::SeedableRng;
+use witchspace::prelude::*;
+
+/// Roll a number of dice
+#[derive(Parser)]
+struct Cli {
+    /// The number of six-sided dice to roll
+    dice: usize,
+    /// Number of sides on the dice
+    sides: u32,
+    /// A seed for the PRNG
+    #[arg(long)]
+    seed: Option<String>,
+    /// Sum the results
+    #[arg(short, long)]
+    sum: bool,
+}
 
 fn main() {
-    let seed = "Aamidal";
-    let mut rng1: Pcg64 = rng_from_seed(seed);
-    println!("{}, {}", rng1.roll(10), rng1.flux());
+    let args = Cli::parse();
+    let mut rng: Pcg64 = match args.seed {
+        Some(seed) => rng_from_seed(seed),
+        None => Pcg64::from_entropy(),
+    };
+    let results: String = if args.sum {
+        rng.roll(args.dice, args.sides).to_string()
+    } else {
+        rng.roll_dice(args.dice, args.sides)
+            .iter()
+            .map(|&id| id.to_string() + ", ")
+            .collect()
+    };
+
+    println!("rolling {} dice, got: {}", args.dice, results)
 }
